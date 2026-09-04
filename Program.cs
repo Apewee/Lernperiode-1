01cfefc58002f779c0fbd52fc985376c590e1ec0
Apewee <Apewee@tuta.io>
2026-09-04 17:15:39 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+using var client = new HttpClient();
+client.BaseAddress = new Uri ("https://gameinfo.albiononline.com/api/gameinfo")
+
+    Console.Write("spielername");
+string name = Console.ReadLine ();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("ungueltiger Name.");
+    return;
+}
+var search = await client.GetAsync($"/search?q={name}");
+if (!search.IsSuccessStatusCode)
+{
+    Console.WriteLine("Fehler bei der Suche");
+    return;
+}
